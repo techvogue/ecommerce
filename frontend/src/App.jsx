05d9e1de64b63abbaa1343158eafc3455cbb9a7b
techvogue <vishwagautam57@gmail.com>
2025-03-27 05:54:55 +0530

    correcting few errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
-import Contact from './pages/Contact'
+
 import Cart from './pages/Cart'
 import Product from './pages/Product'
 import Orders from './pages/Orders'
